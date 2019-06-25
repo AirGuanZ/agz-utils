64ddd113cf06234d229e8d65a9749b9acbfaf9b0
Z Guan <airguanz@gmail.com>
2019-06-25 18:00:45 +0800

    fix ce

--- src/include/agz/utility/console/pbar.h
+++ src/include/agz/utility/console/pbar.h
@@ -6,120 +6,129 @@
 namespace agz::console
 {
 
-    class ProgressBar
+/**
+ * @brief 适用于终端字符界面的进度条
+ * 
+ * 基本使用流程：构造时确定总任务数、进度条长度以及用于显示的字符
+ * 使用display来重绘进度条
+ * 使用done来关闭进度条
+ * 使用operator++使已完成的任务数+1
+ */
+class ProgressBar
+{
+    int finished_;
+    int total_;
+
+    int width_;
+    char complete_;
+    char incomplete_;
+
+    std::chrono::steady_clock::time_point start_ = std::chrono::steady_clock::now();
+
+public:
+
+    ProgressBar(int total, int width, char complete = '#', char incomplete = ' ')
+        : finished_(0), total_(total), width_(width), complete_(complete), incomplete_(incomplete)
     {
-        int finished_;
-        int total_;
 
-        int width_;
-        char complete_;
-        char incomplete_;
+    }
 
-        std::chrono::steady_clock::time_point start_ = std::chrono::steady_clock::now();
-
-    public:
-
-        ProgressBar(int total, int width, char complete = '#', char incomplete = ' ')
-            : finished_(0), total_(total), width_(width), complete_(complete), incomplete_(incomplete)
-        {
-
-        }
-
-        ProgressBar &operator++()
-        {
-            ++finished_;
-            if(finished_ >= total_)
-                done();
-            return *this;
-        }
-
-        void display() const
-        {
-            float progress = (float)finished_ / total_;
-            int pos = (int)(width_ * progress);
-
-            std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
-            auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_).count();
-
-            std::cout << "[";
-
-            for(int i = 0; i < width_; ++i)
-            {
-                if(i < pos)
-                    std::cout << complete_;
-                else if(i == pos)
-                    std::cout << ">";
-                else
-                    std::cout << incomplete_;
-            }
-            std::cout << "] " << int(progress * 100.0) << "% "
-                << float(time_elapsed) / 1000.0 << "s\r";
-            std::cout.flush();
-        }
-
-        void done()
-        {
-            display();
-            if(finished_ <= total_)
-            {
-                std::cout << std::endl;
-                ++finished_;
-            }
-        }
-    };
-
-    class ProgressBarF
+    ProgressBar &operator++()
     {
-        float percent_;
+        ++finished_;
+        return *this;
+    }
 
-        int width_;
-        char complete_;
-        char incomplete_;
+    void display() const
+    {
+        float progress = (float)finished_ / total_;
+        int pos = (int)(width_ * progress);
 
-        std::chrono::steady_clock::time_point start_ = std::chrono::steady_clock::now();
+        std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+        auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_).count();
 
-    public:
+        std::cout << "[";
 
-        ProgressBarF(int width, char complete = '#', char incomplete = ' ')
-            : percent_(0), width_(width), complete_(complete), incomplete_(incomplete)
+        for(int i = 0; i < width_; ++i)
         {
-
+            if(i < pos)
+                std::cout << complete_;
+            else if(i == pos)
+                std::cout << ">";
+            else
+                std::cout << incomplete_;
         }
+        std::cout << "] " << int(progress * 100.0) << "% "
+            << float(time_elapsed) / 1000.0 << "s\r";
+        std::cout.flush();
+    }
 
-        void set_percent(float percent)
+    void done()
+    {
+        display();
+        if(finished_ <= total_)
         {
-            percent_ = percent;
-        }
-
-        void display() const
-        {
-            int pos = (int)(width_ * percent_ / 100);
-
-            std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
-            auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_).count();
-
-            std::cout << "[";
-
-            for(int i = 0; i < width_; ++i)
-            {
-                if(i < pos)
-                    std::cout << complete_;
-                else if(i == pos)
-                    std::cout << ">";
-                else
-                    std::cout << incomplete_;
-            }
-            std::cout << "] " << int(percent_) << "% "
-                << float(time_elapsed) / 1000.0 << "s\r";
-            std::cout.flush();
-        }
-
-        void done()
-        {
-            display();
             std::cout << std::endl;
+            ++finished_;
         }
-    };
+    }
+};
+
+/**
+ * @brief 基本同ProgressBar，只不过直接以已完成的百分比为参数
+ */
+class ProgressBarF
+{
+    float percent_;
+
+    int width_;
+    char complete_;
+    char incomplete_;
+
+    std::chrono::steady_clock::time_point start_ = std::chrono::steady_clock::now();
+
+public:
+
+    ProgressBarF(int width, char complete = '#', char incomplete = ' ')
+        : percent_(0), width_(width), complete_(complete), incomplete_(incomplete)
+    {
+
+    }
+
+    void set_percent(float percent)
+    {
+        percent_ = percent;
+    }
+
+    void display() const
+    {
+        int pos = (int)(width_ * percent_ / 100);
+
+        std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+        auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_).count();
+
+        std::cout << "[";
+
+        for(int i = 0; i < width_; ++i)
+        {
+            if(i < pos)
+                std::cout << complete_;
+            else if(i == pos)
+                std::cout << ">";
+            else
+                std::cout << incomplete_;
+        }
+        std::cout << "] " << int(percent_) << "% "
+            << float(time_elapsed) / 1000.0 << "s\r";
+        std::cout.flush();
+    }
+
+    void done()
+    {
+        display();
+        std::cout << std::endl;
+    }
+};
 
 } // namespace AGZ
 
--- src/include/agz/utility/system/shell.h
+++ src/include/agz/utility/system/shell.h
@@ -5,6 +5,9 @@
 namespace agz::sys
 {
     
+/**
+ * @brief 使用系统默认打开方式来打开指定文件
+ */
 void open_with_default_app(const std::string &filename);
 
 } // namespace agz::sys
