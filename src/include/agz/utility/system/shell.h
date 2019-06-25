04d29d37bcfca5dc759158f423a8bf35a13a77a8
Z Guan <airguanz@gmail.com>
2019-06-25 13:55:55 +0800

    add agz::sys::open_with_default_app

---
+++ src/include/agz/utility/system/shell.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace agz::sys
+{
+    
+void open_with_default_app(const std::string &filename);
+
+} // namespace agz::sys
---
+++ src/src/system/shell.cpp
@@ -0,0 +1,22 @@
+#include <agz/utility/misc/platform.h>
+#include <agz/utility/system/shell.h>
+
+#ifdef AGZ_OS_WIN32
+#   include <Windows.h>
+#elif defined(AGZ_OS_LINUX)
+#   include <cstdlib>
+#endif
+
+namespace agz::sys
+{
+    
+void open_with_default_app(const std::string &filename)
+{
+#ifdef AGZ_OS_WIN32
+    ShellExecuteA(0, 0, filename.c_str(), 0, 0, SW_SHOW);
+#elif defined(AGZ_OS_LINUX)
+    std::system(("xdg-open " + filename).c_str());
+#endif
+}
+
+} // namespace agz::sys
