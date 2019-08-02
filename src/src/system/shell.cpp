ce91dff30970316ef8022812bee163f0f006463c
AirGuanZ <airguanz@gmail.com>
2019-08-02 14:20:09 +0800

    fix ce

--- src/src/system/shell.cpp
+++ src/src/system/shell.cpp
@@ -15,7 +15,7 @@ void open_with_default_app(const std::string &filename)
 #ifdef AGZ_OS_WIN32
     ShellExecuteA(0, 0, filename.c_str(), 0, 0, SW_SHOW);
 #elif defined(AGZ_OS_LINUX)
-    std::system(("xdg-open " + filename).c_str());
+    [[maybe_unused]] int dummy_ret = std::system(("xdg-open " + filename).c_str());
 #endif
 }
 
