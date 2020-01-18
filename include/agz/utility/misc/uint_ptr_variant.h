38c8f7d092cf559d22b9ba8024b7eb2b784e0958
Z Guan <airguanz@gmail.com>
2020-01-19 00:39:32 +0800

    fix ce with stdlibc++

--- include/agz/utility/misc/uint_ptr_variant.h
+++ include/agz/utility/misc/uint_ptr_variant.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <optional>
+#include <memory>
 #include <type_traits>
 
 namespace agz::misc
--- include/agz/utility/texture/impl/texture3d.inl
+++ include/agz/utility/texture/impl/texture3d.inl
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include <cstring>
+
 namespace agz::texture
 {
 
