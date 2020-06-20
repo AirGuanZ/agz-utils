145e5fc3bdd2c61c16ba9afd04d88384777b5240
Z Guan <airguanz@gmail.com>
2020-06-20 16:06:47 +0800

    add misc::direct_inserter

--- include/agz/utility/misc/push_inserter.h
+++ include/agz/utility/misc/push_inserter.h
@@ -51,15 +51,69 @@ namespace misc_impl
             return *this;
         }
     };
+    
+    template <class Container>
+    class direct_insert_iterator
+    {
+    protected:
+    
+        Container *container_;
+    
+    public:
+    
+        using iterator_category = std::output_iterator_tag;
+        using value_type        = void;
+        using difference_type   = void;
+        using pointer           = void;
+        using reference         = void;
+        using container_type    = Container;
+    
+        explicit direct_insert_iterator(Container &c) noexcept
+            : container_(&c)
+        {
+            
+        }
+        
+        direct_insert_iterator<Container> &operator=(
+            typename Container::const_reference value)
+        {
+            container_->insert(value);
+            return *this;
+        }
+    
+        direct_insert_iterator<Container> &operator*()
+        {
+            return *this;
+        }
+        
+        direct_insert_iterator<Container> &operator++()
+        {
+            return *this;
+        }
+        
+        direct_insert_iterator<Container> operator++(int)
+        {
+            return *this;
+        }
+    };
 }
 
 /**
- * @brief 创建一个调用容器的push方法的inserter iterator
+ * @brief 创建一个调用容器的push方法的output iterator
  */
 template<typename Container>
 misc_impl::push_insert_iterator<Container> push_inserter(Container &container)
 {
     return misc_impl::push_insert_iterator<Container>(container);
+}
+
+/**
+ * @brief 创建一个调用容器的insert方法的output iterator
+ */
+template<typename Container>
+misc_impl::direct_insert_iterator<Container> direct_inserter(Container &container)
+{
+    return misc_impl::direct_insert_iterator<Container>(container);
 }
 
 } // namespace agz::misc
