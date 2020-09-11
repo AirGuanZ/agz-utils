8f48b492f8374562a0c363f26498e708295cd656
Z Guan <airguanz@gmail.com>
2020-09-11 19:46:00 +0800

    faster scope guard

--- include/agz/utility/misc/scope_guard.h
+++ include/agz/utility/misc/scope_guard.h
@@ -38,6 +38,33 @@ public:
     }
 };
 
+template<typename T, typename = std::enable_if_t<std::is_invocable_v<T>>>
+class fixed_scope_guard_t : public uncopyable_t
+{
+    bool call_ = true;
+
+    T func_;
+
+public:
+
+    explicit fixed_scope_guard_t(T &&func)
+        : func_(std::forward<T>(func))
+    {
+        
+    }
+
+    ~fixed_scope_guard_t()
+    {
+        if(call_)
+            func_();
+    }
+
+    void dismiss()
+    {
+        call_ = false;
+    }
+};
+
 /**
  * @brief 定义一个自动命名的scope guard，离开定义作用域时自动调用指定的操作
  * 
@@ -49,6 +76,6 @@ public:
 #define AGZ_SCOPE_GUARD_IMPL0(X, LINE) \
     AGZ_SCOPE_GUARD_IMPL1(X, LINE)
 #define AGZ_SCOPE_GUARD_IMPL1(X, LINE) \
-    ::agz::misc::scope_guard_t _auto_scope_guard##LINE([&] X)
+    ::agz::misc::fixed_scope_guard_t _auto_scope_guard##LINE([&] X)
 
 } // namespace agz::misc
